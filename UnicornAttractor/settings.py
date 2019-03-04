810fa12a3b5b45c62fca42a52a90562309000f5c
Brent Molenberghs <brent.molenberghs@it4itservices.com>
2019-03-04 15:58:17 +0100

    Revert "Revert "Revert "heroku"""
    This reverts commit c0adc18cd11f41abc3eac63c2801a020529b6ef4.

--- UnicornAttractor/settings.py
+++ UnicornAttractor/settings.py
@@ -119,11 +119,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
-
-PROJECT_DIR = os.path.dirname(__file__)
-
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-
+STATIC_ROOT = '/static/'
 
 STATIC_URL = '/static/'
 
